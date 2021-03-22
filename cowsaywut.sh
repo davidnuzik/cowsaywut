627cb252ae4bbb56a4833f901a49ed0261475cc7
David Nuzik <david.nuzik@rancher.com>
2021-03-22 13:54:11 -0700

    progress
    * Started with an if statement that is now a function for reuse
    * Use a for loop to loop thru prereqs
    * If we want to not halt when there is a failure and report on which
      packages are not in path more work is needed

--- cowsaywut.sh
+++ cowsaywut.sh
@@ -4,17 +4,46 @@
 # Created by David Nuzik. Just for fun.                       #
 #                                                             #
 # This script requires cowsay, fortune, and lolcat packages   #
-# Intended for use on distros that use apt (apt-get)          #
+#                                                             #
 ###############################################################
 
 ## Todos:
-## add logic to ensure have pre-reqs (all packages used)
+## perform a check that the tty can handle colors (for lolcat)
 ## include argument to set speed of animations and set a var to specify lolcat speed
 
 ## Notes:
 ## lolcat default -s seped is 20. 40 is kinda fast 60 or more very fast.
 ## lolcat default -d (animation duration) is 12. Us 2 for better performance such as on a pi zero.
 ###############################################################
+
+# Check that cowsay, lolcat, and fortune can be invoked
+printf "\nChecking that all pre-reqs exist on PATH...\n\n" && sleep .5
+
+## Declare function to check all pre-reqs
+function which_check {
+which $1 >/dev/null 2>&1
+
+if [ $? -ne 0 ] #check for non-zero exit code (check if the package is not on path)
+  then
+    # is true, therefore the prereq is NOT on path (not good)
+    echo "$prereq IS NOT ON PATH (NOT GOOD)."
+    sleep .5
+  else
+    # is false, therefore the prereq is on path (good)
+    echo "$prereq gave us a zero exit code... (this is good)."
+    sleep .5
+fi
+}
+
+## Invoke which_check function in a for loop for each prereq (package) we need
+for prereq in cowsay fortune lolcat
+  do
+    echo "Checking $prereq..." && which_check $prereq && sleep 1
+done
+
+# temporary pause and EXIT 199 to express completion of for loop / function.
+sleep 3
+exit 199
 
 # clear terminal window
 clear
