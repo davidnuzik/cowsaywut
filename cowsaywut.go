1c37eb8a1962f1f3b8e57d77887d4417b9f353ba
David Nuzik <david.nuzik@rancher.com>
2021-03-25 16:40:50 -0700

    small progress
    Signed-off-by: David Nuzik <david.nuzik@rancher.com>

--- cowsaywut.go
+++ cowsaywut.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Mooo!")
+	fmt.Println("DEBUG: Entrypoint.")
+	out, err := exec.Command("fortune").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Your fortune is: %s\n", out)
 }
